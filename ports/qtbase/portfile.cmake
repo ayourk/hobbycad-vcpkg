d87f11855ee9895b7fbc6a1b204210810afd48f6
Aaron Yourk <ayourk@gmail.com>
2026-03-03 14:28:28 -0700

    Add qtbase 6.4.2 port for macOS static builds
    Uses PPA dfsg tarball to ensure consistent Qt version across platforms.
    Includes patch to fix macOS ARM64 build failure in no_direct_extern_access
    config test (visibility protected is ELF-specific, not supported on Mach-O).
    
    Minimal configuration for HobbyCAD needs:
    - Core, GUI, Widgets, OpenGL modules
    - Static linking
    - System dependencies (freetype, harfbuzz, etc.)
    
    Signed-off-by: Aaron Yourk <ayourk@gmail.com>

---
+++ ports/qtbase/portfile.cmake
@@ -0,0 +1,152 @@
+# Qt 6 Base Module for HobbyCAD static builds
+# Source: PPA dfsg tarball (hosted on GitHub releases)
+
+set(QT_VERSION 6.4.2)
+
+vcpkg_download_distfile(ARCHIVE
+    URLS
+        "https://github.com/ayourk/hobbycad-vcpkg/releases/download/sources/qt6-base_${QT_VERSION}+dfsg.orig.tar.xz"
+    FILENAME "qt6-base_${QT_VERSION}+dfsg.orig.tar.xz"
+    SHA512 2704b90dab05ad2bc31a1171e2818aaa694d5d579d0defe27f9806f3d6c6263467c0673415b3fa73612e45f076442b5fe003d79b9eb758489616263153189a8d
+)
+
+vcpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+    SOURCE_BASE "qtbase-everywhere-src-${QT_VERSION}"
+    PATCHES
+        fix-macos-visibility-test.patch
+)
+
+vcpkg_find_acquire_program(PERL)
+get_filename_component(PERL_PATH ${PERL} DIRECTORY)
+vcpkg_add_to_path(${PERL_PATH})
+
+vcpkg_find_acquire_program(PYTHON3)
+get_filename_component(PYTHON3_PATH ${PYTHON3} DIRECTORY)
+vcpkg_add_to_path(${PYTHON3_PATH})
+
+# Feature options for HobbyCAD's needs
+set(FEATURE_OPTIONS
+    -DFEATURE_concurrent=ON
+    -DFEATURE_dbus=OFF
+    -DFEATURE_gui=ON
+    -DFEATURE_network=OFF
+    -DFEATURE_opengl=ON
+    -DFEATURE_printsupport=OFF
+    -DFEATURE_sql=OFF
+    -DFEATURE_testlib=OFF
+    -DFEATURE_widgets=ON
+    -DFEATURE_xml=ON
+)
+
+# Dependency configuration
+set(INPUT_OPTIONS
+    -DINPUT_doubleconversion=system
+    -DINPUT_freetype=system
+    -DINPUT_harfbuzz=system
+    -DINPUT_libjpeg=system
+    -DINPUT_libpng=system
+    -DINPUT_pcre=system
+)
+
+# Platform-specific options
+set(PLATFORM_OPTIONS "")
+if(VCPKG_TARGET_IS_WINDOWS)
+    list(APPEND PLATFORM_OPTIONS
+        -DFEATURE_opengl_desktop=ON
+    )
+elseif(VCPKG_TARGET_IS_OSX)
+    list(APPEND PLATFORM_OPTIONS
+        -DFEATURE_opengl_desktop=ON
+        -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0
+    )
+endif()
+
+# Disable features not needed
+set(DISABLE_OPTIONS
+    -DCMAKE_DISABLE_FIND_PACKAGE_ATSPI2=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_Cups=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_DirectFB=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_EGL=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_Fontconfig=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_GLESv2=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_GLIB2=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_GSSAPI=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_GTK3=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_ICU=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_Libdrm=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_Libinput=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_Libproxy=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_LTTngUST=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_Mtdev=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_MySQL=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_ODBC=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_Oracle=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_PostgreSQL=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_SQLite3=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_Slog2=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_Tslib=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_Vulkan=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_WrapBrotli=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_WrapDBus1=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_WrapOpenSSL=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_X11=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_XCB=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_XKB=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_ZSTD=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_gbm=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_libb2=ON
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_SHARED_LIBS=OFF
+        -DQT_BUILD_EXAMPLES=OFF
+        -DQT_BUILD_TESTS=OFF
+        -DQT_BUILD_BENCHMARKS=OFF
+        -DQT_USE_BUNDLED_BundledFreetype=OFF
+        -DQT_USE_BUNDLED_BundledHarfbuzz=OFF
+        -DQT_USE_BUNDLED_BundledLibpng=OFF
+        -DQT_USE_BUNDLED_BundledPcre2=OFF
+        -DQT_USE_BUNDLED_BundledZLIB=OFF
+        -DFEATURE_relocatable=ON
+        -DHOST_PERL=${PERL}
+        ${FEATURE_OPTIONS}
+        ${INPUT_OPTIONS}
+        ${PLATFORM_OPTIONS}
+        ${DISABLE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME Qt6 CONFIG_PATH lib/cmake/Qt6)
+vcpkg_cmake_config_fixup(PACKAGE_NAME Qt6Core CONFIG_PATH lib/cmake/Qt6Core)
+vcpkg_cmake_config_fixup(PACKAGE_NAME Qt6CoreTools CONFIG_PATH lib/cmake/Qt6CoreTools)
+vcpkg_cmake_config_fixup(PACKAGE_NAME Qt6Gui CONFIG_PATH lib/cmake/Qt6Gui)
+vcpkg_cmake_config_fixup(PACKAGE_NAME Qt6GuiTools CONFIG_PATH lib/cmake/Qt6GuiTools)
+vcpkg_cmake_config_fixup(PACKAGE_NAME Qt6OpenGL CONFIG_PATH lib/cmake/Qt6OpenGL)
+vcpkg_cmake_config_fixup(PACKAGE_NAME Qt6OpenGLWidgets CONFIG_PATH lib/cmake/Qt6OpenGLWidgets)
+vcpkg_cmake_config_fixup(PACKAGE_NAME Qt6Widgets CONFIG_PATH lib/cmake/Qt6Widgets)
+vcpkg_cmake_config_fixup(PACKAGE_NAME Qt6WidgetsTools CONFIG_PATH lib/cmake/Qt6WidgetsTools)
+
+vcpkg_copy_pdbs()
+
+# Copy tools
+set(QT_TOOLS moc rcc uic)
+vcpkg_copy_tools(TOOL_NAMES ${QT_TOOLS} SEARCH_DIR "${CURRENT_PACKAGES_DIR}/bin" AUTO_CLEAN)
+
+# Clean up
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/lib/cmake"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+    "${CURRENT_PACKAGES_DIR}/lib/cmake"
+)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+# Install license
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSES/LGPL-3.0-only.txt")
